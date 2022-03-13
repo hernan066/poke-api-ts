554dc269814f174d70034d86f0583daf26bd152e
hernangit config --global user.name John <hernan0066@gmail.com>
2022-03-13 11:53:44 -0300

    Detail-Responsive

--- components/pokemon/details/PokeDetails.tsx
+++ components/pokemon/details/PokeDetails.tsx
@@ -36,7 +36,6 @@ export const PokeDetails: FC<Props> = ({ pokemon, species, evolution }) => {
   };
 
   const handleFavorite = () => {
-    
     localFavorites.toggleFavorite(pokemon.id);
     setIsInFavorites(!isInFavorites);
 
@@ -52,111 +51,106 @@ export const PokeDetails: FC<Props> = ({ pokemon, species, evolution }) => {
       },
     });
   };
-  
-  
+
   return (
-    <div className="grid-container">
-      <aside className="sidebar">
-        <div className={`sidebar__card ${type1} `}>
-          <div className="sidebar__img-container">
-            <Image
-              src={`/poke512/${pokemon.id}.png`}
-              height={500}
-              width={500}
-              alt={pokemon.name}
-            />
-          </div>
+    <div className="flex-container">
+      <aside className={`sidebar__card ${type1} `}>
+        <div className="sidebar__img-container">
+          <Image
+            src={`/poke512/${pokemon.id}.png`}
+            height={500}
+            width={500}
+            alt={pokemon.name}
+          />
+        </div>
 
-          <div className="sidebar__type-text">
-            <h3>{pokemon.name}</h3>
-          </div>
+        <div className="sidebar__type-text">
+          <h3>{pokemon.name}</h3>
+        </div>
 
-          <div className="sidebar__img-type-container">
+        <div className="sidebar__img-type-container">
+          <Image
+            src={`/types/${type1}.png`}
+            height={150}
+            width={100}
+            alt={pokemon.name}
+          />
+          {type2 && (
             <Image
-              src={`/types/${type1}.png`}
+              src={`/types/${type2}.png`}
               height={150}
               width={100}
               alt={pokemon.name}
             />
-            {type2 && (
-              <Image
-                src={`/types/${type2}.png`}
-                height={150}
-                width={100}
-                alt={pokemon.name}
-              />
-            )}
-          </div>
+          )}
         </div>
       </aside>
       <main className="main">
-        <div className="espaciador"></div>
-        <div className="main__card">
-          <header className="main__title">
-            <h1>{pokemon.name}</h1>
-          </header>
-          <nav className="main__nav">
-            <div className={`main__stats ${page.bio} ${type1}`}>
-              <h2
-                onClick={() =>
-                  setPage({
-                    bio: "active",
-                    stats: "",
-                    evolution: "",
-                  })
-                }
-              >
-                Bio
-              </h2>
-            </div>
-            <div className={`main__stats ${page.stats} ${type1}`}>
-              <h2
-                onClick={() =>
-                  setPage({
-                    bio: "",
-                    stats: "active",
-                    evolution: "",
-                  })
-                }
-              >
-                Base Stats
-              </h2>
-            </div>
-            <div className={`main__stats ${page.evolution} ${type1}`}>
-              <h2
-                onClick={() =>
-                  setPage({
-                    bio: "",
-                    stats: "",
-                    evolution: "active",
-                  })
-                }
-              >
-                Evolution
-              </h2>
-            </div>
-          </nav>
-          {page.bio === "active" && <Bio species={species} pokemon={pokemon} />}
-          {page.stats === "active" && (
-            <BaseStats pokemon={pokemon} species={species} />
-          )}
-          {page.evolution === "active" && (
-            <Evolution evolution={evolution} pokemon={pokemon} />
-          )}
-          <div className="main__buttons">
-            <div
-              className={`main__button-return  ${type1}`}
-              onClick={handleClick}
+        <header className="main__title">
+          <h1>{pokemon.name}</h1>
+        </header>
+        <nav className="main__nav">
+          <div className={`main__stats ${page.bio} ${type1}`}>
+            <h2
+              onClick={() =>
+                setPage({
+                  bio: "active",
+                  stats: "",
+                  evolution: "",
+                })
+              }
             >
-              Return
-            </div>
-            <div className={`main__button-favorite  ${type1} ${isInFavorites ? 'favorite' : ''}`} 
-            onClick={handleFavorite}>
-            {
-              isInFavorites ? 'Remove from favorites' : 'Add to favorites'
-            }
-              
-            </div>
+              Bio
+            </h2>
+          </div>
+          <div className={`main__stats ${page.stats} ${type1}`}>
+            <h2
+              onClick={() =>
+                setPage({
+                  bio: "",
+                  stats: "active",
+                  evolution: "",
+                })
+              }
+            >
+              Base Stats
+            </h2>
+          </div>
+          <div className={`main__stats ${page.evolution} ${type1}`}>
+            <h2
+              onClick={() =>
+                setPage({
+                  bio: "",
+                  stats: "",
+                  evolution: "active",
+                })
+              }
+            >
+              Evolution
+            </h2>
+          </div>
+        </nav>
+        {page.bio === "active" && <Bio species={species} pokemon={pokemon} />}
+        {page.stats === "active" && (
+          <BaseStats pokemon={pokemon} species={species} />
+        )}
+        {page.evolution === "active" && (
+          <Evolution evolution={evolution} pokemon={pokemon} />
+        )}
+        <div className="main__buttons">
+          <div
+            className={`main__button-return  ${type1}`}
+            onClick={handleClick}
+          >
+            Return
+          </div>
+          <div
+            className={`main__button-favorite  ${type1} ${
+              isInFavorites ? "favorite" : ""
+            }`}
+            onClick={handleFavorite}
+          >
+            {isInFavorites ? "Remove from favorites" : "Add to favorites"}
           </div>
         </div>
       </main>
